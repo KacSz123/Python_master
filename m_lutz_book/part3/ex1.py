bf716f21fdfef7342ce5dde4bbe8b7cf5c5d3e44
Kacper <kacperszcz159@gmail.com>
2022-12-29 13:24:23 +0100

    next step strings

--- m_lutz_book/part3/ex1.py
+++ m_lutz_book/part3/ex1.py
@@ -1,1 +1,14 @@
 #some exercise with string
+s = "siemaJakasiemaJaka"
+s=s.replace("Jaka", "$$$$$")
+print(s)
+s="123"+s
+print(s.find('siema'))
+ss=list(s)
+print(ss)
+s2 = "aaa bbb ccc"
+print(s2.split())
+############ formatowanie operatorami
+
+
+print("ten %d %s jest martwy" % (1, "lew"))
